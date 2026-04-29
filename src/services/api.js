4f2dcfb2b4d6cd2cc30e07b9af010940a0499a6c
wash <washquesia@gmail.com>
2026-04-28 23:01:22 -0300

    Teste

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080";
-
+const API_URL = import.meta.env.VITE_API_URL;
 const api = axios.create({
     baseURL: API_URL,
 });
@@ -9,9 +8,6 @@ const api = axios.create({
 // REQUEST INTERCEPTOR
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
-
-    console.log("➡️ Request:", config.url);
-    console.log("🔑 Token:", token);
 
     if (token && token.trim() !== "") {
         config.headers.Authorization = `Bearer ${token}`;
@@ -24,11 +20,9 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
     (response) => response,
     (error) => {
-        console.log("❌ Erro:", error.response?.status, error.config?.url);
+        console.log("Erro:", error.response?.status, error.config?.url);
 
         if (error.response?.status === 401) {
-            console.warn("⚠️ Não autorizado → logout");
-
             localStorage.removeItem("token");
             window.location.href = "/";
         }
