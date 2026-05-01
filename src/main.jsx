59a2d2082cae8fc44729db191106878483a1dc1d
wash <washquesia@gmail.com>
2026-05-01 16:00:03 -0300

    Atualizaçao de rotina

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,6 @@ import "./index.css";
 import { AuthProvider } from "./auth/AuthContext";
 import { ThemeProvider } from "./context/ThemeContext";
 
-// 🔥 IMPORTANTE: PWA
-import { registerSW } from 'virtual:pwa-register'
-registerSW()
-
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <ThemeProvider>
--- vite.config.js
+++ vite.config.js
@@ -1,50 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: false
-      },
-      manifest: {
-        name: 'Sistema Igreja em Células',
-        short_name: 'Igreja',
-        description: 'Gestão de Igreja em Células',
-        theme_color: '#0d6efd',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-
   ],
-
   server: {
     host: true,
     port: 5173,
     strictPort: true,
     cors: true,
   },
-
   build: {
     chunkSizeWarningLimit: 1000,
   }
